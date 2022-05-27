c2f96a0f2ec43c2e173634d0c6136ca267631995
Sergio Daguil III <89905448+CHOKOLOKI@users.noreply.github.com>
2022-05-27 17:25:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+duwa = Ursina()
+Sky(texture='sky_sunset')
+Tao = FirstPersonController()
+
+class kahon(Button):
+    def __init__(self, position=(0, 0, 0)):
+        super().__init__(
+            parent=scene,
+            position=position,
+            model='cube',
+            origin_y=0.5,
+            texture='grass',
+            color=color.color(0, 0, random.uniform(0.9, 1)),
+            highlight_color=color.lime)
+
+    def input(self, key):
+        if self.hovered:
+            if key == 'right mouse down':
+                ibutang = Voxel(position=self.position + mouse.normal)
+
+            if key == 'left mouse down':
+                destroy(self)
+
+class hand()
+
+
+for z in range(30):
+    for x in range(30):
+        ibutang = kahon(position=(x, 0, z))
+
+duwa.run()
